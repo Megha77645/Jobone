972a9893aa5ed73c94fdf33ed94ffbea6a8bfb30
Megha77645 <meghagupta3569@gmail.com>
2025-08-01 21:32:58 +0530

    added dropdown company

--- jobpost.js
+++ jobpost.js
@@ -4,3 +4,14 @@ document.querySelector('.right-section').addEventListener('submit', function(e) 
   // Optionally, add validation or post logic here
   alert('Job post form submitted!');
 });
+
+function companyNameInput() {
+  const userType = document.getElementById('user_type').value;
+  const companyInputDiv = document.getElementById('companyNameInput');
+
+  if (userType === 'company') {
+    companyInputDiv.classList.remove('hidden');
+  } else {
+    companyInputDiv.classList.add('hidden');
+  }
+}
